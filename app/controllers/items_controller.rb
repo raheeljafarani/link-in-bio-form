481945450b3c5c3a40300f65dd512d60b770f593
raheeljafarani <rjafaran@chicagobooth.edu>
2025-05-02 00:08:22 +0000

    graded build

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,4 +4,20 @@ class ItemsController < ApplicationController
 
     render({ :template => "item_templates/list" })
   end
+
+  def new_link_form
+      render({ :template => "item_templates/new_link_form" })
+  end
+
+  def create_new_link
+    new_item = Item.new
+
+    new_item.link_url = params.fetch("query_link_url")
+    new_item.link_description = params.fetch("query_link_description")
+    new_item.thumbnail_url = params.fetch("query_thumbnail_url")
+
+    new_item.save
+
+    redirect_to("/")
+  end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
 
-  get("/", { :controller => "items", :action => "index" })
+  get("/", { :controller => "items", :action => "index" })  
+
+  get("/backdoor", { :controller => "items", :action => "new_link_form"})
+  post("/insert_item", { :controller => "items", :action => "create_new_link"})
+
 end
